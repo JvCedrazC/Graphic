d8819b76cb76a2738e2b9000b1d9f5b4eeb2b543
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-06-18 17:07:16 -0300

    Create TechnicianDaoList.java

---
+++ src/main/java/com/example/phase4/dao/TechnicianDao/TechnicianDaoList.java
@@ -0,0 +1,102 @@
+package com.example.phase4.dao.TechnicianDao;
+
+
+import com.example.phase4.FilesUtils.ManagingFiles;
+import com.example.phase4.Model.Technician;
+
+import java.util.ArrayList;
+
+public class TechnicianDaoList implements TechnicianDAO{
+    private ManagingFiles managingFiles;
+    private int nextID;
+
+    public TechnicianDaoList(){
+        this.managingFiles = new ManagingFiles("Technician.bin");
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        if (lista.size() == 0){
+            this.nextID = lista.size();
+        } else{
+            this.nextID = 0;
+        }
+    }
+
+    @Override
+    public Technician create(Technician technician){
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        technician.setId(nextID);
+        nextID++;
+        lista.add(technician);
+        save(lista);
+        return technician;
+
+    }
+
+    @Override
+    public Technician findById(int id) {
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        for (Technician technician: lista) {
+            if (technician.getId() == id){
+                return technician;
+            }
+        } return null;
+
+    }
+    @Override
+    public ArrayList<Technician> findMany(){
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        ArrayList<Technician> listTechnician = new ArrayList<Technician>();
+        for (Object o : lista) {
+            listTechnician.add((Technician) o);
+        }
+        return listTechnician;
+    }
+
+    @Override
+    public void update(Technician technician){
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getId() == technician.getId()) {
+                lista.set(i, technician);
+                save(lista);
+                return;
+            }
+        }
+    }
+    @Override
+    public void deleteById(int id){
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getId() == id) {
+                lista.remove(i);
+                break;
+            }
+        }
+        changeId();
+        save(lista);
+    }
+    public void deleteMany(){
+        ArrayList<Technician> lista = new ArrayList<Technician>();
+        this.nextID =  0;
+        save(lista);
+    }
+    public Technician findByName(String name){
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        for (Technician i: lista) {
+            if (i.getName() == name){
+                return i;
+            }
+        }return null;
+    }
+    private void changeId(){
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        for (int i = 0; i < lista.size();i++){
+            lista.get(i).setId(i);
+        }
+        save(lista);
+    }
+    public void save(ArrayList<Technician> lista){
+
+        managingFiles.save(lista);
+    }
+
+}
