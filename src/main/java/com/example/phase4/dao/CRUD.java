3416776fdcdff06eac5862f26cf0b20bec46ffe1
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-06-18 17:07:36 -0300

    Create CRUD.java

---
+++ src/main/java/com/example/phase4/dao/CRUD.java
@@ -0,0 +1,17 @@
+package com.example.phase4.dao;
+
+import java.util.List;
+
+public interface CRUD <T>{
+    //Create
+    public T create(T obj);
+    //Read
+    public T findById(int id);
+    public List<T> findMany();
+    //update
+    public void update(T obj);
+    //delete
+    public void deleteById(int id);
+    public void deleteMany();
+
+}
