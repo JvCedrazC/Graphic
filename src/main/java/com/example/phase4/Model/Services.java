f521e86a8afa2a83e963512ce103f10fda20c7ac
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-06-18 17:08:31 -0300

    Create Services.java

---
+++ src/main/java/com/example/phase4/Model/Services.java
@@ -0,0 +1,32 @@
+package com.example.phase4.Model;
+
+import java.io.Serializable;
+
+public abstract class Services implements Serializable {
+    private Double price;
+    private Double cost;
+    private int workOrderId;
+
+    //price getters and setters
+    public Double getPrice() {
+        return this.price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    //cost getters and setters
+    public Double getCost() {
+        return this.cost;
+    }
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public int getWorkOrderId() {
+        return this.workOrderId;
+    }
+    public void setWorkOrderId(int workOrderId){
+        this.workOrderId = workOrderId;
+    }
+}
