f72c49d42981b38d3e40c89a08cb8ce97212a739
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-06-18 17:08:15 -0300

    Create Customer.java

---
+++ src/main/java/com/example/phase4/Model/Customer.java
@@ -0,0 +1,38 @@
+package com.example.phase4.Model;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+    private int id;
+    private String name;
+    private String email;
+
+    public Customer(int id, String name, String email){
+        setId(id);
+        setName(name);
+        setEmail(email);
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
