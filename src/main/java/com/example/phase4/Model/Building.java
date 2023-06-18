634b123bc63b55282f03322829a930c5d9794c77
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-06-18 17:08:03 -0300

    Create Building.java

---
+++ src/main/java/com/example/phase4/Model/Building.java
@@ -0,0 +1,43 @@
+package com.example.phase4.Model;
+
+import java.util.ArrayList;
+public class Building extends Services {
+    private static ArrayList<Component> usedComponents = new ArrayList<Component>();
+    //Class' Methods
+    public Building (ArrayList<Component> component){
+        for (int i = 0; i < component.size();i++){
+            Component x =addComponent(component.get(i));
+            System.out.println("Component added: " + x.getName()+"\nComponent ID:"+ x.getId());
+        }
+        calculatePrice();
+        calculateCost();
+
+
+    }
+    public static ArrayList<Component> getUsedComponents() {
+        return usedComponents;
+    }
+
+    private Component addComponent(Component component){
+        usedComponents.add(component);
+        return component;
+    }
+    private void calculatePrice(){
+        Double total = 0.0;
+        for (int i = 0; i < usedComponents.size();i++){
+            total += usedComponents.get(i).getPrice();
+        }
+        setPrice(total);
+    }
+    private void calculateCost(){
+        Double total = 0.0;
+        for (int i = 0; i < usedComponents.size();i++){
+            total += usedComponents.get(i).getCost();
+        }
+        setCost(total);
+    }
+    private void defineWorkOrderId(int id){
+        setWorkOrderId(id);
+    }
+
+}
