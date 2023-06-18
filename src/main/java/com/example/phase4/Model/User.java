24ff2454e9a74294febf790bc9aa94bb797f7522
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-06-18 17:08:38 -0300

    Create User.java

---
+++ src/main/java/com/example/phase4/Model/User.java
@@ -0,0 +1,27 @@
+package com.example.phase4.Model;
+
+import java.io.Serializable;
+
+public abstract class User implements Serializable {
+    int id;
+    String name;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    User(int id, String name){
+        setName(name);
+        setId(id);
+    }
+}
